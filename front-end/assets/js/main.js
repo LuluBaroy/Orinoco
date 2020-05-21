86e5c38cb3ee1af15ef17c4fa6e62c6d57fff691
Lulu Baroy <lucie.baroiller@gmail.com>
2020-05-21 10:42:20 +0200

    modif request

--- front-end/assets/js/main.js
+++ front-end/assets/js/main.js
@@ -1,67 +1,67 @@
 //Getting products informations with Promise
 const connection = function(url){
     return new Promise(function (resolve, reject) {
-        let teddies = new XMLHttpRequest();
-        teddies.onreadystatechange = function() {
-            if (teddies.readyState === XMLHttpRequest.DONE){
-               if(teddies.status === 200) {
-                   resolve(JSON.parse(teddies.responseText));
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE){
+               if(request.status === 200) {
+                   resolve(JSON.parse(request.responseText));
                } else {
                    reject();
                }
             }
         }
-        teddies.open("GET", url);
-        teddies.send();
+        request.open("GET", url);
+        request.send();
     })
 }
-//Using btn-teddies to launch the GET method with click event
-let btn = document.getElementById('btn-teddies');
+//Defining different btns state
 let isClicked = false;
-btn.addEventListener('click', function(event) {
+let currentParam = 'teddies';
+function getItems(param) {
+    //Creating the list container
+    let sectionProducts = document.createElement('section');
+        sectionProducts.id = "container-list";
 
-    //Verifying btn-teddies hasn't already been clicked
-    if(isClicked === false) {
-        connection("http://localhost:3000/api/teddies").then(function (response) {
+    if(isClicked === false || currentParam != param){
+        if(isClicked === true && currentParam != param){
+            document.getElementById('container-list').remove();
+        }
+        document.getElementById('homepage').appendChild(sectionProducts);
+        connection("http://localhost:3000/api/" + param).then(function (response) {
 
-            //Getting all products informations then stocking it into 'products' variable
-            let products = [];
-            products = response;
+             //Getting all products informations then stocking it into 'products' variable
+             let products = [];
+                 products = response;
 
-            //For each products, we create HTML structure
-            products.forEach(function (response) {
-                let sectionProducts = document.createElement('section');
-                let linkProductsPage = document.createElement('a');
-                let idProduct = response._id;
-                linkProductsPage.href = 'pages/products.html?id=' + idProduct;
-                linkProductsPage.ariaLabel = "Page du produit";
-                let imageProducts = document.createElement('img');
-                imageProducts.src = response.imageUrl;
-                imageProducts.alt = "Photo Ourson " + response.name;
-                imageProducts.title = "Photo de présentation ourson " + response.name;
-                let divProducts = document.createElement('div');
-                let titleProducts = document.createElement('h2');
-                titleProducts.textContent = response.name;
-                let descriptionProducts = document.createElement('p');
-                descriptionProducts.textContent = response.description;
+             //For each products, we create HTML structure
+             products.forEach(function (response) {
+                 let articleProducts = document.createElement('article');
+                 let linkProductsPage = document.createElement('a');
+                 let idProduct = response._id;
+                     linkProductsPage.href = 'pages/products.html?id=' + idProduct;
+                     linkProductsPage.ariaLabel = "Page du produit";
+                 let imageProducts = document.createElement('img');
+                     imageProducts.src = response.imageUrl;
+                     imageProducts.alt = "Photo Ourson " + response.name;
+                     imageProducts.title = "Photo de présentation ourson " + response.name;
+                 let divProducts = document.createElement('div');
+                 let titleProducts = document.createElement('h2');
+                     titleProducts.textContent = response.name;
+                 let descriptionProducts = document.createElement('p');
+                     descriptionProducts.textContent = response.description;
 
-                //Placing new elements on the HTML page
-                document.getElementById('homepage').appendChild(sectionProducts);
-                sectionProducts.appendChild(linkProductsPage);
-                linkProductsPage.appendChild(imageProducts);
-                sectionProducts.appendChild(divProducts);
-                divProducts.appendChild(titleProducts);
-                divProducts.appendChild(descriptionProducts);
+                 //Placing new elements on the index page
+                 sectionProducts.appendChild(articleProducts);
+                 articleProducts.appendChild(linkProductsPage);
+                 linkProductsPage.appendChild(imageProducts);
+                 articleProducts.appendChild(divProducts);
+                 divProducts.appendChild(titleProducts);
+                 divProducts.appendChild(descriptionProducts);
 
-                //As isCliked passed to true, the function won't run again
-                isClicked = true;
-
-            });
+                 isClicked = true;
+             });
         }).catch();
-    } else {
-        //Warning the users about why nothing's happening
-        alert('Les articles sont déjà chargés ! Si vous ne les voyez pas il y a probablement une erreur de chargement serveur !');
     }
-});
-
+}
 
--- front-end/assets/js/products.js
+++ front-end/assets/js/products.js
@@ -4,52 +4,57 @@ let queryStr = window.location.search;
 let urlStr = new URLSearchParams(queryStr);
 let idProducts = urlStr.get('id');
 
+//Getting products informations with Promise
 const connection = function(url){
     return new Promise(function (resolve, reject) {
-        let teddies = new XMLHttpRequest();
-        teddies.onreadystatechange = function() {
-            if (teddies.readyState === XMLHttpRequest.DONE){
-                if(teddies.status === 200) {
-                    resolve(JSON.parse(teddies.responseText));
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE){
+                if(request.status === 200) {
+                    resolve(JSON.parse(request.responseText));
                 } else {
                     reject();
                 }
             }
         }
-        teddies.open("GET", url);
-        teddies.send();
+        request.open("GET", url);
+        request.send();
     })
 }
 
-//Getting products informations
+//Calling of the promise
 connection('http://localhost:3000/api/teddies').then(function (response) {
+    //Creating a variable to stock response
     let products = [];
     products = response;
+
+    //For each products, we're creating the HTML structure
     products.forEach(function (response) {
         if (response._id === idProducts) {
-            //Product Page
             let sectionProducts = document.createElement('section');
 
             let imageProducts = document.createElement('img');
-            imageProducts.src = response.imageUrl;
-            imageProducts.alt = "Photo Ourson " + response.name;
-            imageProducts.title = "Photo de présentation ourson " + response.name;
+                imageProducts.src = response.imageUrl;
+                imageProducts.alt = "Photo Ourson " + response.name;
+                imageProducts.title = "Photo de présentation ourson " + response.name;
 
             let divProducts = document.createElement('div');
 
             let titleProducts = document.createElement('h1');
-            titleProducts.textContent = response.name;
+                titleProducts.textContent = response.name;
 
             let refProduct = document.createElement('p');
-            refProduct.textContent = "Ref: " + response._id;
+                refProduct.textContent = "Ref: " + response._id;
 
             let descriptionProducts = document.createElement('p');
-            descriptionProducts.textContent = response.description;
+                descriptionProducts.textContent = response.description;
 
             let labelSelect = document.createElement('label');
                 labelSelect.for = "color-select";
                 labelSelect.textContent = "Couleur : ";
             let selectProduct = document.createElement('select');
+
+            //Getting all color options
             function getAllOptions() {
                 response.colors.forEach((value, index) => {
                     let option = document.createElement('option');
@@ -59,17 +64,43 @@ connection('http://localhost:3000/api/teddies').then(function (response) {
                 })
             }
 
+            //Converting the price in euro
             let priceProduct = document.createElement('p');
             let priceLength = response.price.toString();
             priceProduct.textContent = `Prix : ${priceLength[0]}${priceLength[1]}.${priceLength[2]}${priceLength[3]} €`;
 
+            //Possibility to choose quantity
+            let quantity = document.createElement('label');
+                quantity.for = "quantity";
+                quantity.textContent = "Quantité : ";
+                quantity.className = "selectQuantity";
+            let selectQuantity = document.createElement('select');
+                selectQuantity.name = "quantity";
+                selectQuantity.id = "quantityChoose";
+
+            function optionQuantity(){
+                let j = 0;
+                while(j<=8){
+                    j++;
+                    let option = document.createElement('option');
+                    option.textContent = j;
+                    option.value = j;
+                    selectQuantity.appendChild(option);
+                }
+            }
+            //Creating the select element by calling the optionQuantity function
+            optionQuantity();
+
+            //Basket button
             let buttonBasket = document.createElement('button');
             buttonBasket.textContent = 'Ajouter au panier';
-            buttonBasket.addEventListener('onclick', function () {
+            buttonBasket.addEventListener('click', function (event) {
 
             });
 
-            getAllOptions();
+            getAllOptions();    //Calling of the option function
+
+            //Placing all elements in the product page
             document.getElementById('productPage').appendChild(sectionProducts);
             sectionProducts.appendChild(imageProducts);
             sectionProducts.appendChild(divProducts);
@@ -78,10 +109,11 @@ connection('http://localhost:3000/api/teddies').then(function (response) {
             divProducts.appendChild(descriptionProducts);
             divProducts.appendChild(labelSelect);
             divProducts.appendChild(selectProduct);
+            divProducts.appendChild(quantity);
+            quantity.appendChild(selectQuantity);
             divProducts.appendChild(priceProduct);
             divProducts.appendChild(buttonBasket);
         }
     })
 })
-
 
