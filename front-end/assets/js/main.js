5aee4d125ed3bebf186161b1cadb966543d8b947
Lulu Baroy <lucie.baroiller@gmail.com>
2020-05-28 12:46:45 +0200

    MàJ panier

--- front-end/assets/js/main.js
+++ front-end/assets/js/main.js
@@ -17,19 +17,23 @@ const connection = function(url){
 }
 
 //Getting Product ID
-let linkOfPage = window.location.href;
 let queryStr = window.location.search;
 let urlStr = new URLSearchParams(queryStr);
-let idProducts = urlStr.get('id');
 let currentParamProducts = urlStr.get('type');
+
 let productsAdded = 0;
 if(JSON.parse(localStorage.getItem('cart')) === null){
     productsAdded = 0;
 } else {
     productsAdded = JSON.parse(localStorage.getItem('cart')).length;
 }
+function modifyingHeader(id, root){
+    document.getElementById(id).innerHTML = "<img class=\"basket\" src=" + root + "assets/img/basket.png" + "> Panier ( " + productsAdded + " )";
+}
+
 
 function priceCalculation(price, priceText, text) {
    let euros = price.toString().slice(0, price.toString().length - 2);
     priceText.textContent = text + new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format(euros);
+    return euros;
 }
--- front-end/assets/js/products.js
+++ front-end/assets/js/products.js
@@ -1,153 +1,136 @@
-//Modifying the Header if products were added on other pages
-if(productsAdded > 0){
-    document.getElementById('btn-basket-products').innerHTML = "<img class=\"basket\" src=\"../assets/img/basket.png\"> Panier ( " + productsAdded + " )";
-}
-
 //Calling of the promise
-connection('http://localhost:3000/api/' + currentParamProducts).then(function (response) {
+connection('http://localhost:3000/api/' + currentParamProducts + "/" + urlStr.get("id")).then(function (response) {
     //Creating a variable to stock response
     let products = [];
     products = response;
 
-    //For each product, we're creating the HTML structure
-    products.forEach(function (response) {
-        let sectionProducts = document.createElement('section');
-        let buttonCart = document.createElement('button');
-        buttonCart.textContent = 'Ajouter au panier';
+    let sectionProducts = document.createElement('section');
+    //Parametring the buttonCart
+    let buttonCart = document.createElement('button');
+    buttonCart.textContent = 'Ajouter au panier';
+    buttonCart.addEventListener('click', function (event) {
 
-        //If the ID got in 'products' is the same of the URL's param
-        if (response._id === idProducts) {
-            let imageProducts = document.createElement('img');
-                imageProducts.src = response.imageUrl;
-                imageProducts.alt = "Photo "+ currentParamProducts + " " +response.name;
-                imageProducts.title = "Photo de présentation " + currentParamProducts+ " " + response.name;
-            let divProducts = document.createElement('div');
-            let titleProducts = document.createElement('h1');
-                titleProducts.textContent = response.name;
-            let refProduct = document.createElement('p');
-                refProduct.textContent = "Ref: " + response._id;
-            let descriptionProducts = document.createElement('p');
-                descriptionProducts.textContent = response.description;
-            let labelSelect = document.createElement('label');
-                labelSelect.for = "option-product";
-                labelSelect.textContent = "Options : ";
-            let selectProduct = document.createElement('select');
+        //Getting the locally stored item and verifying if this is the first product added
+        let cartUp = JSON.parse(localStorage.getItem('cart'));
 
-            //Switching option property according to URL's param
-            let firstProperty = "";
-            switch (currentParamProducts){
-                case "teddies":
-                    firstProperty = 'colors';
-                    break;
-                case "cameras":
-                    firstProperty = 'lenses';
-                    break;
-                case "furniture":
-                    firstProperty = "varnish";
-                    break;
-            }
-
-            //Getting all options
-            function getAllOptions(value) {
-                response[value].forEach((value, index) => {
-                    let option = document.createElement('option');
-                    let optionValue = document.createTextNode(value);
-                    option.appendChild(optionValue);
-                    selectProduct.appendChild(option);
-                })
-            }
-
-            //Calling of the option function with the switch value as parameter
-            getAllOptions(firstProperty);
-
-            let priceProduct = document.createElement('p');
-            let priceLength = response.price;
-
-            //Converting the price in euro
-            priceCalculation(priceLength, priceProduct, `Prix : `);
-
-            //Possibility to choose quantity
-            let quantity = document.createElement('label');
-                quantity.for = "quantity";
-                quantity.textContent = "Quantité : ";
-                quantity.className = "selectQuantity";
-            let selectQuantity = document.createElement('select');
-                selectQuantity.name = "quantity";
-                selectQuantity.id = "quantityChoose";
-
-             //Creating possibility to choose quantity of each products
-            function optionQuantity(){
-                let j = 0;
-                while(j<=8){
-                    j++;
-                    let option = document.createElement('option');
-                    option.textContent = j;
-                    option.value = j;
-                    selectQuantity.appendChild(option);
+        if(cartUp === null){                  //if this is the first product added, creating new line and stocking it
+            let cart = [];
+            let firstLine = new Line(currentParamProducts, response.imageUrl, response.name, response._id, parseInt(quantityChoose.value), response.price);
+            cart.push(firstLine);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            location.reload();
+        } else {                              //Else, verifying if the product has already been added
+            let cartUp2 = JSON.parse(localStorage.getItem('cart'));
+            let productAlreadyAdded = false;
+            for (let k in cartUp2) {
+                if (cartUp2[k].id === response._id) {
+                    productAlreadyAdded = true;
+                    cartUp2[k].quantity = parseInt(cartUp2[k].quantity) + parseInt(quantityChoose.value);
                 }
             }
-            //Creating the 'select' element by calling the optionQuantity function
-            optionQuantity();
-
-            //Placing all elements in the product page
-            document.getElementById('productPage').appendChild(sectionProducts);
-            sectionProducts.appendChild(imageProducts);
-            sectionProducts.appendChild(divProducts);
-            divProducts.appendChild(titleProducts);
-            divProducts.appendChild(refProduct);
-            divProducts.appendChild(descriptionProducts);
-            divProducts.appendChild(labelSelect);
-            divProducts.appendChild(selectProduct);
-            divProducts.appendChild(quantity);
-            quantity.appendChild(selectQuantity);
-            divProducts.appendChild(priceProduct);
-            divProducts.appendChild(buttonCart);
+            if (!productAlreadyAdded) {
+                cartUp2.push(new Line(currentParamProducts, response.imageUrl, response.name, response._id, parseInt(quantityChoose.value), response.price));
+            }
+            localStorage.setItem('cart', JSON.stringify(cartUp2));
+            location.reload();
         }
+    });
 
-        //Creating a new Class to add different products easily
-        class Line{
-            constructor(param, imgUrl, name, id, quantity, price){
-                this.param = param;
-                this.imgUrl = imgUrl;
-                this.name = name;
-                this.id = id;
-                this.quantity = quantity;
-                this.price = price;
-            }
+    let imageProducts = document.createElement('img');
+        imageProducts.src = response.imageUrl;
+        imageProducts.alt = "Photo "+ currentParamProducts + " " +response.name;
+        imageProducts.title = "Photo de présentation " + currentParamProducts+ " " + response.name;
+    let divProducts = document.createElement('div');
+    let titleProducts = document.createElement('h1');
+        titleProducts.textContent = response.name;
+    let refProduct = document.createElement('p');
+        refProduct.textContent = "Ref: " + response._id;
+    let descriptionProducts = document.createElement('p');
+        descriptionProducts.textContent = response.description;
+    let labelSelect = document.createElement('label');
+        labelSelect.for = "option-product";
+        labelSelect.textContent = "Options : ";
+    let selectProduct = document.createElement('select');
+
+    //Switching option property according to URL's param
+    let firstProperty = "";
+    switch (currentParamProducts){
+        case "teddies":
+            firstProperty = 'colors';
+            break;
+        case "cameras":
+            firstProperty = 'lenses';
+            break;
+        case "furniture":
+            firstProperty = "varnish";
+            break;
+    }
+
+    //Getting all options
+    function getAllOptions(value) {
+        response[value].forEach((value, index) => {
+            let option = document.createElement('option');
+            let optionValue = document.createTextNode(value);
+            option.appendChild(optionValue);
+            selectProduct.appendChild(option);
+        })
+    }
+
+    //Calling of the option function with the switch value as parameter
+    getAllOptions(firstProperty);
+
+    let priceProduct = document.createElement('p');
+    let priceLength = response.price;
+    //Converting the price in euro
+    priceCalculation(priceLength, priceProduct, `Prix : `);
+
+    //Possibility to choose quantity
+    let quantity = document.createElement('label');
+    quantity.for = "quantity";
+    quantity.textContent = "Quantité : ";
+    quantity.className = "selectQuantity";
+    let selectQuantity = document.createElement('select');
+    selectQuantity.name = "quantity";
+    selectQuantity.id = "quantityChoose";
+
+    //Creating possibility to choose quantity of each products
+    function optionQuantity(){
+        let j = 0;
+        while(j<=8){
+            j++;
+            let option = document.createElement('option');
+            option.textContent = j;
+            option.value = j;
+            selectQuantity.appendChild(option);
         }
-        //Parametring the buttonCart
-        buttonCart.addEventListener('click', function (event) {
+    }
+    //Creating the 'select' element by calling the optionQuantity function
+    optionQuantity();
 
-            //Modifying Header when clicking on the buttonCart
-            document.getElementById('btn-basket-products').innerHTML = "<img class=\"basket\" src=\"../assets/img/basket.png\"> Panier ( " + productsAdded + " )";
+    //Placing all elements in the product page
+    document.getElementById('productPage').appendChild(sectionProducts);
+    sectionProducts.appendChild(imageProducts);
+    sectionProducts.appendChild(divProducts);
+    divProducts.appendChild(titleProducts);
+    divProducts.appendChild(refProduct);
+    divProducts.appendChild(descriptionProducts);
+    divProducts.appendChild(labelSelect);
+    divProducts.appendChild(selectProduct);
+    divProducts.appendChild(quantity);
+    quantity.appendChild(selectQuantity);
+    divProducts.appendChild(priceProduct);
+    divProducts.appendChild(buttonCart);
 
-            //Getting the locally stored item and verifying if this is the first product added
-            let cartUp = JSON.parse(localStorage.getItem('cart'));
-
-            if(cartUp === null){                  //if this is the first product added, creating new line and stocking it
-                alert('Produit ajouté au panier !');
-                let cart = [];
-                let firstLine = new Line(currentParamProducts, response.imageUrl, response.name, response._id, parseInt(quantityChoose.value), response.price);
-                cart.push(firstLine);
-                localStorage.setItem('cart', JSON.stringify(cart));
-                location.reload();
-            } else {                              //Else, verifying if the product has already been added
-                let cartUp2 = JSON.parse(localStorage.getItem('cart'));
-                let productAlreadyAdded = false;
-                for (let k in cartUp2) {
-                    if (cartUp2[k].id === response._id) {
-                        productAlreadyAdded = true;
-                        cartUp2[k].quantity = parseInt(cartUp2[k].quantity) + parseInt(quantityChoose.value);
-                        alert('La quantité a été augmentée pour ce produit !');
-                    }
-                }
-                if (!productAlreadyAdded) {
-                    cartUp2.push(new Line(currentParamProducts, response.imageUrl, response.name, response._id, parseInt(quantityChoose.value), response.price));
-                    alert('Nouveau produit ajouté !');
-                }
-                localStorage.setItem('cart', JSON.stringify(cartUp2));
-                location.reload();
-            }
-        });
-    })
+    //Creating a new Class to add different products easily in the localStorage item
+    class Line{
+        constructor(param, imgUrl, name, id, quantity, price){
+            this.param = param;
+            this.imgUrl = imgUrl;
+            this.name = name;
+            this.id = id;
+            this.quantity = quantity;
+            this.price = price;
+        }
+    }
 })
+modifyingHeader('btn-cart-products', "../");
