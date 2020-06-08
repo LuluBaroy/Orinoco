28b6d34919186c3a1baa28e012410539facbcd27
Lulu Baroy <lucie.baroiller@gmail.com>
2020-06-08 14:23:55 +0200

    installation webpack

--- front-end/assets/js/suggestions.js
+++ front-end/assets/js/suggestions.js
@@ -36,8 +36,6 @@ if (productsAddedToCart === null) {
 
         }
     }
-    let productAlreadySuggested = false;
-    let newArray;
     function suggestionsProducts() {
         for(let i = 0; i <= 2; i++) {
             let randomParam = Math.floor(Math.random() * 3);
@@ -55,48 +53,90 @@ if (productsAddedToCart === null) {
             }
             connection("http://localhost:3000/api/" + paramChose).then(function (response) {
                 let randomProducts = Math.floor(Math.random() * 5);
-                switch (randomProducts) {
-                    case 0:
-                        arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[0]._id, response[0].name, response[0].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[0]._id, response[0].price));
-                        break;
-                    case 1:
-                        arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[1]._id, response[1].name, response[1].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[1]._id, response[1].price));
-                        break;
-                    case 2:
-                        arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[2]._id, response[2].name, response[2].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[2]._id, response[2].price));
-                        break;
-                    case 3:
-                        arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[3]._id, response[3].name, response[3].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[3]._id, response[3].price));
-                        break;
-                    case 4:
-                        arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[4]._id, response[4].name, response[4].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[4]._id, response[4].price));
-                        break;
+                if(arrayForProductRandomlySelected.length === 0){
+                    switch (randomProducts) {
+                        case 0:
+                            arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[0]._id, response[0].name, response[0].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[0]._id, response[0].price));
+                            break;
+                        case 1:
+                            arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[1]._id, response[1].name, response[1].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[1]._id, response[1].price));
+                            break;
+                        case 2:
+                            arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[2]._id, response[2].name, response[2].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[2]._id, response[2].price));
+                            break;
+                        case 3:
+                            arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[3]._id, response[3].name, response[3].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[3]._id, response[3].price));
+                            break;
+                        case 4:
+                            arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[4]._id, response[4].name, response[4].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[4]._id, response[4].price));
+                            break;
+                    }
+                } else {
+                    let productsRandomId;
+                    switch (randomProducts) {
+                        case 0:
+                            productsRandomId = response[0]._id;
+                            break;
+                        case 1:
+                            productsRandomId = response[1]._id;
+                            break;
+                        case 2:
+                            productsRandomId = response[2]._id;
+                            break;
+                        case 3:
+                            productsRandomId = response[3]._id;
+                            break;
+                        case 4:
+                            productsRandomId = response[4]._id;
+                            break;
+                    }
+                    if(arrayForProductRandomlySelected.length === 1 && arrayForProductRandomlySelected[0].id !== productsRandomId || arrayForProductRandomlySelected.length === 2 && arrayForProductRandomlySelected[0].id !== productsRandomId && arrayForProductRandomlySelected[1].id !== productsRandomId) {
+                        switch (productsRandomId) {
+                            case response[0]._id:
+                                arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[0]._id, response[0].name, response[0].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[0]._id, response[0].price));
+                                break;
+                            case response[1]._id:
+                                arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[1]._id, response[1].name, response[1].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[1]._id, response[1].price));
+                                break;
+                            case response[2]._id:
+                                arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[2]._id, response[2].name, response[2].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[2]._id, response[2].price));
+                                break;
+                            case response[3]._id:
+                                arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[3]._id, response[3].name, response[3].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[3]._id, response[3].price));
+                                break;
+                            case response[4]._id:
+                                arrayForProductRandomlySelected.push(new ProductsRandomlySelected(paramChose, response[4]._id, response[4].name, response[4].imageUrl, 'products.html?type=' + paramChose + '&id=' + response[4]._id, response[4].price));
+                                break;
+                        }
+                    }
+                    console.log(arrayForProductRandomlySelected);
                 }
+
             });
         }
-            //Creating the visualisations' layout
-            /*let articleSuggested = document.createElement('article');
-            articleSuggested.className = 'articleSuggested';
-            let nameOfProduct = document.createElement('h3');
-            nameOfProduct.textContent = titleProductChose;
-            let imgProductSuggested = document.createElement('img');
-            imgProductSuggested.src = imgSrc;
-            let linkToProduct = document.createElement('a');
-            linkToProduct.href = linkProduct;
-            let priceText = document.createElement('p');
-            let priceProductsSuggested = priceOfProduct;
-            priceCalculation(priceProductsSuggested, priceText, 'Prix : ');
+        //Creating the visualisations' layout
+        /*let articleSuggested = document.createElement('article');
+        articleSuggested.className = 'articleSuggested';
+        let nameOfProduct = document.createElement('h3');
+        nameOfProduct.textContent = titleProductChose;
+        let imgProductSuggested = document.createElement('img');
+        imgProductSuggested.src = imgSrc;
+        let linkToProduct = document.createElement('a');
+        linkToProduct.href = linkProduct;
+        let priceText = document.createElement('p');
+        let priceProductsSuggested = priceOfProduct;
+        priceCalculation(priceProductsSuggested, priceText, 'Prix : ');
 
-            //Placing new elements in cart page
-            divProductsSuggested.appendChild(articleSuggested);
-            articleSuggested.appendChild(nameOfProduct);
-            articleSuggested.appendChild(linkToProduct);
-            linkToProduct.appendChild(imgProductSuggested);
-            articleSuggested.appendChild(priceText);*/
-        
+        //Placing new elements in cart page
+        divProductsSuggested.appendChild(articleSuggested);
+        articleSuggested.appendChild(nameOfProduct);
+        articleSuggested.appendChild(linkToProduct);
+        linkToProduct.appendChild(imgProductSuggested);
+        articleSuggested.appendChild(priceText);*/
+
     }
     suggestionsProducts();
-    console.log(arrayForProductRandomlySelected);
+
 
 }
 //Calling the random suggestions (1 by product)
